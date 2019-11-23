cbe92e700e87d8331688350d0853a4efbc19bcc8
Nick <sshedl22@gmail.com>
2019-11-23 15:19:04 +0300

    Can not generate pdf with unicode

---
+++ src/main/java/ReportGen.java
@@ -0,0 +1,62 @@
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.html.simpleparser.HTMLWorker;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.awt.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class ReportGen {
+
+
+    static void createHTML(String srcXmlFile, String srcXsltFile, String outputFileName) throws TransformerException, IOException {
+        // creating html from xml with xslt
+        /* Create a TransformerFactory object */
+        FileOutputStream out = new FileOutputStream(outputFileName);
+        TransformerFactory tFactory = TransformerFactory.newInstance();
+        /* Get the incoming XSLT file */
+        Transformer transformer = tFactory.newTransformer(new StreamSource(srcXsltFile));
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        out.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n".getBytes(StandardCharsets.UTF_8));
+        /* Get the XML file and apply the XSLT transformation to convert to HTML */
+        transformer.transform(new StreamSource(srcXmlFile), new StreamResult(out));
+
+    }
+
+    static void createPDF(String srcXmlFile, String srcXsltFile, String outputFileName) throws TransformerException, IOException, DocumentException {
+        createHTML(srcXmlFile,srcXsltFile,outputFileName);
+        String url = new File(outputFileName).toURI().toURL().toString();
+        System.out.println(""+url);
+        String HTML_TO_PDF = "ConvertedFile.pdf";
+        OutputStream os = new FileOutputStream(HTML_TO_PDF);
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.getFontResolver().addFont("Fonts\\arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        renderer.setDocument(url);
+        renderer.layout();
+        renderer.createPDF(os);
+        os.close();
+
+//        createHTML(srcXmlFile,srcXsltFile,outputFileName);
+//        Document document = new Document();
+//        // Создаем writer для записи в pdf
+//        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("output.pdf"));
+//        // Открываем для чтения html страничку
+//        document.open();
+//        // Парсим её и записываем в PDF
+//        XMLWorkerHelper.getInstance().parseXHtml(writer, document, new FileInputStream(outputFileName));
+//        document.close();
+    }
+
+    public static void main(String[] args) throws TransformerException, IOException, DocumentException {
+        //unicode????
+        createPDF("table.xml", "template.xslt", "sample.html");
+
+    }
+}
